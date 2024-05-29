ede9035c970f1eadc735bd8a2135079ab97528d6
Lessly <leslyzuniga.zc@gmail.com>
2024-05-29 18:22:25 -0500

    WIP:resposive movil redesign

--- src/components/molecules/Tag/Tag.tsx
+++ src/components/molecules/Tag/Tag.tsx
@@ -4,23 +4,23 @@ import "./Tag.css";
 const Tag = () => {
   return (
     <div className="tag-container">
-        <div className="vertical-text-container">
-          <p className="vertical-text">COVERED</p>
+      <div className="vertical-text-container">
+        <p className="vertical-text">COVERED</p>
+      </div>
+      <div className="content">
+        <div className="yellow-circle">
+          <p className="dollar">$</p>
         </div>
-        <div className="content">
-          <div className="yellow-circle">
-            <p className="dollar">$</p>
+        <div style={{ position: "absolute" }}>
+          <div className="big-text">
+            <p>500k</p>
           </div>
-          <div style={{ position: "absolute" }}>
-            <div className="big-text">
-              <p>500k</p>
-            </div>
-            <div className="small-text">
-              <p>$10.99/month</p>
-            </div>
+          <div className="small-text">
+            <p>$10.99/month</p>
           </div>
         </div>
       </div>
+    </div>
   );
 };
 
